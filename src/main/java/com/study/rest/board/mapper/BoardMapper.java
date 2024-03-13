4a4afa0af2987ebfdc451c7f90139eed0109d290
lovi0714 <lovi0714@gmail.com>
2024-03-13 17:40:25 +0900

    조회 기능

--- src/main/java/com/study/rest/board/mapper/BoardMapper.java
+++ src/main/java/com/study/rest/board/mapper/BoardMapper.java
@@ -1,7 +1,12 @@
 package com.study.rest.board.mapper;
 
+import com.study.rest.board.dto.Board;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface BoardMapper {
+
+    List<Board> getBoardList();
 }
--- src/main/java/com/study/rest/board/service/BoardService.java
+++ src/main/java/com/study/rest/board/service/BoardService.java
@@ -1,12 +1,19 @@
 package com.study.rest.board.service;
 
+import com.study.rest.board.dto.Board;
 import com.study.rest.board.mapper.BoardMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class BoardService {
 
     private final BoardMapper boardMapper;
+
+    public List<Board> getBoardList() {
+        return boardMapper.getBoardList();
+    }
 }
